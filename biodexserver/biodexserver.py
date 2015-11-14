7c39460823b8cf733c1180fb7831691a77d13dc4
Kevin <kchen508@gmail.com>
2015-11-14 12:45:14 -0800

    biodexserver.py

--- biodexserver/biodexserver.py
+++ biodexserver/biodexserver.py
@@ -1,16 +1,28 @@
 import tornado.ioloop
 import tornado.web
+import sqlite3
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
+class EntryHandler(tornado.web.RequestHandler):
+    def post(self):
+        self.write("hello")
+        
+class PicturesHandler(tornado.web.RequestHandler):
+    def get(self, id, name):
+        self.write("Hello " + name + " with id " + id)
+
 def main():
     return tornado.web.Application([
         (r"/", MainHandler),
+	(r"/addEntry", EntryHandler),
+        (r"/pictures/([0-9]+)/([a-z]+)", PicturesHandler), 
     ])
 
 if __name__ == "__main__":
+    db = sqlite3.connect('biodex')
     app = main()
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
