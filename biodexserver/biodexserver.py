1e53e57b4513af13f46996e4c432a9df8399b50d
Kevin <kchen508@gmail.com>
2015-11-15 06:24:56 -0800

    did a bunch of changes to stuff

--- biodexserver/biodexserver.py
+++ biodexserver/biodexserver.py
@@ -1,6 +1,9 @@
 import tornado.ioloop
 import tornado.web
 import sqlite3
+from PIL import Image
+import StringIO
+import math
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -13,29 +16,73 @@ class EntryHandler(tornado.web.RequestHandler):
         file_body = file1['body']
         img = Image.open(StringIO.StringIO(file_body))
         img.save(f_name)
-        db = sqlite3.connect('biodex')
+        #print(self.request.body)
+        name = self.get_argument('name', None)
+        latitude = self.get_argument('latitude', 0)
+        longitude = self.get_argument('longitude', 0)
+        description = self.get_argument('description', None)
+        db = sqlite3.connect('biodex.db')
         cursor = db.cursor()
-        cursor.execute("IF COL_LENGTH('picture_table', 'name_column') IS NOT NULL")
-        
-class PicturesHandler(tornado.web.RequestHandler):
-    def get(self, name, item_id):
-        self.write("Hello " + name + " with id " + item_id)
-        db = sqlite3.connect('biodex')
-        c = db.cursor()
-# Add primary key into table      
-        try:
-            c.execute("SELECT {pt} ({ic}, {nc}), VALUES (item_id, name)".\
-                format(pt=picture_table, ic=id_column, nc=name_column))
-        except sqlite3.IntegrityError:
-            print('ERROR: ID already exists in PRIMARY KEY column {}'.format(id_column))      
+        cursor.execute('INSERT INTO picture_table VALUES (?, ?, ?, ?, ?, ?)', (None, name, float(latitude), float(longitude), f_name, description,))
         db.commit()
         db.close()
         
+class PicturesHandler(tornado.web.RequestHandler):
+    def get(self, name, item_id):
+        db = sqlite3.connect('biodex.db')
+        c = db.cursor()
+        c.execute("SELECT file_path FROM picture_table WHERE id = ?", (item_id,))
+        f_path = c.fetchone()[0]
+        print(f_path)
+        ifile = open(f_path, "r")
+        self.set_header('Content-Type', 'image/png')
+        self.set_header('Content-Disposition', 'attach; filename='+f_path+'')
+        self.write(ifile.read())
+# Add primary key into table      
+        #try:
+        #    c.execute("SELECT {pt} ({ic}, {nc}), VALUES (item_id, name)".\
+        #        format(pt=picture_table, ic=id_column, nc=name_column))
+        #except sqlite3.IntegrityError:
+        #    print('ERROR: ID already exists in PRIMARY KEY column {}'.format(id_column))    
+        #db.commit()
+        db.close()
+
+class PictureRangeHandler(tornado.web.RequestHandler):
+    def get(self, latitude, longitude, m_range):
+        #self.write("I'm at " + latitude + " " + longitude + " and searching in a " + m_range + " mile radius")
+        lat_range = float(m_range) / 68.70749821
+        long_range = float(m_range) / (69.1710411 * math.cos(float(latitude)))
+        max_lat = float(latitude) + lat_range
+        min_lat = float(latitude) - lat_range
+        max_long = float(longitude) + long_range
+        min_long = float(longitude) - long_range
+        true_max_lat = max(max_lat, min_lat)
+        true_min_lat = min(max_lat, min_lat)
+        true_max_long = max(max_long, min_long)
+        true_min_long = min(max_long, min_long)
+        #self.write("my lat range is " + str(true_max_lat) + " and my long is " + str(true_max_long))
+        db = sqlite3.connect('biodex.db')
+        c = db.cursor()
+        c.execute("SELECT * FROM picture_table WHERE latitude BETWEEN ? AND ? AND longitude BETWEEN ? AND ?", (true_min_lat, true_max_lat, true_min_long, true_max_long,))
+        for row in c:
+            self.write(str(row))
+        db.close()
+
+class GetImageInfoHandler(tornado.web.RequestHandler):
+    def get(self, id):
+        db = sqlite3.connect('biodex.db')
+        c = db.cursor()
+        c.execute("SELECT * FROM picture_table WHERE id = ?", (id,))
+        self.write(c.fetchone())
+        db.close()
+
 def main():
     return tornado.web.Application([
         (r"/", MainHandler),
-	(r"/addEntry/", EntryHandler),
-        (r"/pictures/(.+)/([0-9]+)", PicturesHandler), 
+	(r"/addEntry", EntryHandler),
+        (r"/pictures/(.+)/([0-9]+)", PicturesHandler),
+        (r"/picturerange/([-.0-9][^a-z\s]+)/([-.0-9][^a-z\s]+)/([.0-9][^a-z\s]+)", PictureRangeHandler),
+        (r"/getimageinfo/([0-9]+)", GetImageInfoHandler),
     ])
 
 if __name__ == "__main__":
@@ -45,9 +92,9 @@ if __name__ == "__main__":
     column_type1 = 'INTEGER'
     name_column = 'name_column'
     column_type2 = 'TEXT'
-    db = sqlite3.connect('biodex')
+    db = sqlite3.connect('biodex.db')
     c = db.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS {pt} ({ic} {ct1} PRIMARY KEY)'.format(pt=picture_table, ic=id_column, ct1=column_type1))
+    c.execute('''CREATE TABLE IF NOT EXISTS picture_table (id INTEGER PRIMARY KEY, name TEXT, latitude REAL, longitude REAL, file_path TEXT, description TEXT)''')
     db.commit()
     db.close()
     app = main()
